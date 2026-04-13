039bf5a4cf3ee38c8d209720d0dcaca245579450
turinastudio <turinastudio@gmail.com>
2026-04-13 14:13:36 -0300

    refactor: extract shared anime genre options to constant

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -1,3 +1,7 @@
+const ANIME_GENRE_OPTIONS = [
+  "accion", "artes-marciales", "aventura", "carreras", "ciencia-ficcion", "comedia", "demencia", "demonios", "deportes", "drama", "ecchi", "escolares", "espacial", "fantasia", "harem", "historico", "infantil", "josei", "juegos", "magia", "mecha", "militar", "misterio", "musica", "parodia", "policia", "psicologico", "recuentos-de-la-vida", "romance", "samurai", "seinen", "shoujo", "shounen", "sobrenatural", "superpoderes", "suspenso", "terror", "vampiros", "yaoi", "yuri"
+];
+
 export const manifest = {
   id: "com.stremio.web.scraper",
   version: "0.1.1",
@@ -39,7 +43,7 @@ export const manifest = {
       extra: [
         {
           name: "genre",
-          options: ["accion", "artes-marciales", "aventura", "carreras", "ciencia-ficcion", "comedia", "demencia", "demonios", "deportes", "drama", "ecchi", "escolares", "espacial", "fantasia", "harem", "historico", "infantil", "josei", "juegos", "magia", "mecha", "militar", "misterio", "musica", "parodia", "policia", "psicologico", "recuentos-de-la-vida", "romance", "samurai", "seinen", "shoujo", "shounen", "sobrenatural", "superpoderes", "suspenso", "terror", "vampiros", "yaoi", "yuri"],
+          options: ANIME_GENRE_OPTIONS,
           optionsLimit: 1, isRequired: true
         },
         { name: "skip", isRequired: false }
@@ -50,7 +54,7 @@ export const manifest = {
       extra: [
         {
           name: "genre",
-          options: ["accion", "artes-marciales", "aventura", "carreras", "ciencia-ficcion", "comedia", "demencia", "demonios", "deportes", "drama", "ecchi", "escolares", "espacial", "fantasia", "harem", "historico", "infantil", "josei", "juegos", "magia", "mecha", "militar", "misterio", "musica", "parodia", "policia", "psicologico", "recuentos-de-la-vida", "romance", "samurai", "seinen", "shoujo", "shounen", "sobrenatural", "superpoderes", "suspenso", "terror", "vampiros", "yaoi", "yuri"],
+          options: ANIME_GENRE_OPTIONS,
           optionsLimit: 1, isRequired: true
         },
         { name: "skip", isRequired: false }
@@ -72,7 +76,7 @@ export const manifest = {
       extra: [
         {
           name: "genre",
-          options: ["accion", "artes-marciales", "aventura", "carreras", "ciencia-ficcion", "comedia", "demencia", "demonios", "deportes", "drama", "ecchi", "escolares", "espacial", "fantasia", "harem", "historico", "infantil", "josei", "juegos", "magia", "mecha", "militar", "misterio", "musica", "parodia", "policia", "psicologico", "recuentos-de-la-vida", "romance", "samurai", "seinen", "shoujo", "shounen", "sobrenatural", "superpoderes", "suspenso", "terror", "vampiros", "yaoi", "yuri"],
+          options: ANIME_GENRE_OPTIONS,
           optionsLimit: 1, isRequired: true
         },
         { name: "skip", isRequired: false }
